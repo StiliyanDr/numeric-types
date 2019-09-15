58b6caa6256bfd5086e71b6b38e6730c5bc5dc8c
StiliyanDr <stiliyan.dragnev@gmail.com>
2019-09-15 17:22:52 +0300

    Add tests for Rational's arithmetic operations

--- Unit tests/Rational Tests/Rational Tests.cpp
+++ Unit tests/Rational Tests/Rational Tests.cpp
@@ -258,3 +258,91 @@ TEST_CASE("Rational negation and reciprocal",
         REQUIRE_THROWS_AS(reciprocalOf(zero), std::logic_error);
     }
 }
+
+TEST_CASE("Rational arithmetic operations",
+          "[arithmetic]")
+{
+    const auto zero = Rational<>{ 0 };
+    const auto one = Rational<>{ 1 };
+
+    SECTION("Binary operators")
+    {
+        SECTION("With neutral elements")
+        {
+            const auto r = Rational<>{ 1, 2 };
+
+            CHECK((r + zero) == r);
+            CHECK((zero + r) == r);
+            CHECK((r - zero) == r);
+            CHECK((zero - r) == -r);
+            CHECK((r * one) == r);
+            CHECK((one * r) == r);
+            CHECK((r / one) == r);
+            CHECK((one / r) == reciprocalOf(r));
+        }
+
+        SECTION("Keep rationals reduced with sign in numerator")
+        {
+            const auto minusOneThird = Rational<>{ -1, 3 };
+
+            CHECK((Rational<>{ -2, 3 } + minusOneThird) == -one);
+            CHECK((minusOneThird - Rational<>{ 2, 3 }) == -one);
+            CHECK((Rational<>{ 3 } * minusOneThird) == -one);
+            CHECK((minusOneThird / Rational<>{ 1, 3 }) == -one);
+        }
+
+        SECTION("Zero division throws exception")
+        {
+            REQUIRE_THROWS_AS(one / zero, std::logic_error);
+        }
+    }
+
+    SECTION("Compound assignment operators")
+    {
+        SECTION("With neutral elements")
+        {
+            const auto oneThird = Rational<>{ 1, 3 };
+            auto r = oneThird;
+
+            REQUIRE((r += zero) == oneThird);
+            REQUIRE((r -= zero) == oneThird);
+            REQUIRE((r *= one) == oneThird);
+            REQUIRE((r /= one) == oneThird);
+        }
+
+        SECTION("Keep rationals reduced with sign in numerator")
+        {
+            auto minusFourThirds = Rational<>{ -4, 3 };
+
+            SECTION("Addition")
+            {
+                REQUIRE((minusFourThirds += Rational<>{ 1, 3 }) == -one);
+            }
+
+            SECTION("Subtraction")
+            {
+                REQUIRE((minusFourThirds -= Rational<>{ 2, 3 }) ==
+                        Rational<>{ -2 });
+            }
+
+            SECTION("Multiplication")
+            {
+                REQUIRE((minusFourThirds *= Rational<>{ 3 }) ==
+                        Rational<>{ -4 });
+            }
+
+            SECTION("Division")
+            {
+                REQUIRE((minusFourThirds /= Rational<>{ 1, 3 }) ==
+                        Rational<>{ -4 });
+            }
+        }
+
+        SECTION("Zero division throws exception")
+        {
+            auto r = one;
+
+            REQUIRE_THROWS_AS(r /= zero, std::logic_error);
+        }
+    }
+}
